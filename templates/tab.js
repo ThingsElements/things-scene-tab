f394f803a53570ec8e691847a34f8bf4b1bea941
Hearty Oh <heartyoh@hatiolab.com>
2018-03-05 18:54:52 +0900

    fix component template spec.

--- templates/tab.js
+++ templates/tab.js
@@ -1,25 +1,20 @@
+import icon from '../assets/tab.png';
+
 export default {
-  name: 'tab',
-    /* 다국어 키 표현을 어떻게.. */
-  description: '...',
-  /* 다국어 키 표현을 어떻게.. */
-  group: 'container',
-  /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
-  icon: '../',
-  /* 또는, Object */
-  template: {
+  type: 'tab',
+  description: 'container tab',
+  group: 'container', /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
+  icon,
+  model: {
     type: 'tab',
-    model: {
-      type: 'tab',
-      left: 100,
-      top: 100,
-      width: 100,
-      height: 400,
-      lineWidth: 5,
-      fillStyle: 'navy',
-      activeFillStyle: 'red',
-      strokeStyle: 'white',
-      fontColor: 'white'
-    }
+    left: 100,
+    top: 100,
+    width: 100,
+    height: 400,
+    lineWidth: 5,
+    fillStyle: 'navy',
+    activeFillStyle: 'red',
+    strokeStyle: 'white',
+    fontColor: 'white'
   }
 }
